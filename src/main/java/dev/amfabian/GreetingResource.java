52dcadc437b412492879658b1a8839027bea2c2a
Alexandre Fabian <xandy.fabian@gmail.com>
2022-11-04 19:00:53 -0300

    update on workflow

--- src/main/java/dev/amfabian/GreetingResource.java
+++ src/main/java/dev/amfabian/GreetingResource.java
@@ -8,7 +8,6 @@ import javax.ws.rs.core.MediaType;
 @Path("/hello")
 public class GreetingResource {
     //coment
-    //second line comment
     @GET
     @Produces(MediaType.TEXT_PLAIN)
     public String hello() {
